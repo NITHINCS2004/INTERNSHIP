d33f7678f01d4196df38fac835a38dc126383017
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-03-03 13:27:31 +0530

    changes

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'gen-lang-client-0724194648',
+  location: 'asia-south1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'gen-lang-client-0724194648',
+  location: 'asia-south1'
+};
+exports.connectorConfig = connectorConfig;
+
